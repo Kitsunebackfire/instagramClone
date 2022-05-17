acbec1679367d8818cbc33b11600b41910ffe064
Kitsunebackfire <kwolz92@gmail.com>
2022-05-17 14:17:25 -0500

    finish grid styling on userPagePhotos.css and remove guide borders

--- src/routes/UserPage.js
+++ src/routes/UserPage.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Header from "../components/Header";
 import "./UserPage.css";
-import { useSelector } from "react-redux";
 import UserPageBody from "../components/UserPageComponents/UserPageBody";
 
 const UserPage = () => {
-  const displayName = useSelector((state) => state.loginInfo.displayName);
-
   return (
-    <div>
+    <div className="app">
       <Header />
       <div className="userPage__body">
         <UserPageBody />
